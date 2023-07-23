52ae028078ef3f9d5ea71ed9d00535ab92d6be4e
skim411 <skim411@aucklanduni.ac.nz>
2023-07-23 17:13:12 +1200

    update tic tac toe game

--- index.js
+++ index.js
@@ -36,19 +36,49 @@ function cellClicked(){
     checkWinner();
 }
 
-function updateCell(cell, index) {
+function updateCell(cell, index){
     options[index] = currentPlayer;
     cell.textContent = currentPlayer;
 }
 
-function changePlayer() {
-
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 }
 
 function checkWinner() {
+    let roundWon = false;
 
+    for(let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == "") {
+            continue;
+        }
+        if (cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if(roundWon) {
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false; 
+    } else if(!options.includes("")) {
+        statusText.textContent = `Draw`;
+        running = false; 
+    } else {
+        changePlayer();
+    }
 }
 
 function restartGame() {
-
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
 }
